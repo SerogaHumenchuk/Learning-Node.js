78a6140b04403e209d5b7e83587458cf1d483f07
SerogaHumenchuk <niva1999niva@gmail.com>
2019-10-27 01:02:41 +0300

    Resolve auth: add routes, middlewares, connect db

---
+++ AUTH/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () => console.log('connected to DB!'));
+
+// Middlewares
+app.use(express.json());
+// Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server up and running'));
