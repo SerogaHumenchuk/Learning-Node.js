e901eb1615b818a54cb853761a84bc8fcb1b2a64
SerogaHumenchuk <niva1999niva@gmail.com>
2019-10-31 17:36:53 +0200

    add Joi validate

--- AUTH/index.js
+++ AUTH/index.js
@@ -9,11 +9,11 @@ const authRoute = require('./routes/auth');
 dotenv.config();
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () => console.log('connected to DB!'));
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () => console.log('connected to DB!'));
 
 // Middlewares
 app.use(express.json());
 // Route Middlewares
 app.use('/api/user', authRoute);
 
-app.listen(3000, () => console.log('Server up and running'));
+app.listen(5000, () => console.log('Server up and running'));
